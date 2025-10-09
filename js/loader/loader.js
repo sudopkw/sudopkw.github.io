c72e6e28f9c3b5e78a8678499d92c5a1550b68d7
Pkw <104768274+sudopkw@users.noreply.github.com>
2025-10-09 05:35:45 +0200

    Update loader.js

--- js/loader/loader.js
+++ js/loader/loader.js
@@ -1,17 +1,1 @@
-// Simple loader that just loads the files and lets the script handle initialization
-const IS_DEV = window.location.hostname.includes("localhost") || 
-               window.location.hostname.includes("127.0.0.1");
-
-// Load CSS
-const css = document.createElement("link");
-css.rel = "stylesheet";
-css.href = IS_DEV ? "css/tui.css" : "css/tui.min.css";
-document.head.appendChild(css);
-
-// Remove old style if exists
-document.getElementById("min-style")?.remove();
-
-// Load JS - let it handle its own initialization
-const js = document.createElement("script");
-js.src = IS_DEV ? "js/tui.js" : "js/tui.min.js";
-document.head.appendChild(js);
+const IS_DEV="localhost"===window.location.hostname||"127.0.0.1"===window.location.hostname,CSS_PATH=IS_DEV?"css/tui.css":"css/tui.min.css",JS_PATH=IS_DEV?"js/tui.js":"js/tui.min.js";function loadDependency(e,t,n){let s=document.head,c=document.getElementById("min-style"),i="script"===t?document.createElement("script"):document.createElement("link");"script"===t?(i.type="text/javascript",i.src=e):(i.rel="stylesheet",i.href=e),"script"===t&&null!=n&&(i.onreadystatechange=n,i.onload=n),s.appendChild(i),c?.remove()}loadDependency(CSS_PATH,"link"),loadDependency(JS_PATH,"script",function(){init()});
