a60308f4afd60cc23eed6d5f80c816c502c421f9
Pkw <104768274+sudopkw@users.noreply.github.com>
2025-10-10 06:06:59 +0200

    Update visitor.js

--- api/visitor.js
+++ api/visitor.js
@@ -1,1 +1,37 @@
+let visitorCount = 0;
+const visitedIPs = new Set();
 
+export default function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const visitorIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
+  if (!visitedIPs.has(visitorIP)) {
+
+    visitedIPs.add(visitorIP);
+    visitorCount++;
+    console.log(`New visitor: ${visitorIP}, Total: ${visitorCount}`);
+  }
+
+  if (req.method === 'GET') {
+    res.status(200).json({ 
+      count: visitorCount,
+      message: 'Visitor count retrieved'
+    });
+  } else if (req.method === 'POST') {
+    res.status(200).json({ 
+      count: visitorCount,
+      message: 'Visitor tracked'
+    });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
